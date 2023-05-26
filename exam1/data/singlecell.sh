e99bf5763b9d068c2dd22a96829230f946ab38e4
davidcalderon27 <117690532+davidcalderon27@users.noreply.github.com>
2023-05-26 17:58:16 -0500

    Update singlecell.sh

--- exam1/data/singlecell.sh
+++ exam1/data/singlecell.sh
@@ -1,3 +1,3 @@
-find ./data/singlecell  
+
 find ./data/singlecell  -type f \( -name "*.csv" -o -name "*.txt" -o -name "*.tsv" -o -name "*.tsv" -o -name "*.gz" -o -name "*.png" \) 
 grep -E -o '\[ a-z] >> "extensiones.txt"
